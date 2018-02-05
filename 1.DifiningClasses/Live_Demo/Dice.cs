7715bcaba60e8228d505832bf96a709f1682a396
VladislavPenchev <vgpenchev@gmail.com>
2018-02-05 07:54:18 +0200

    implement Dice

---
+++ 1.DifiningClasses/Live_Demo/Dice.cs
@@ -0,0 +1,29 @@
+﻿namespace Live_Demo
+{
+    using System;
+
+    public class Dice
+    {
+        private int _sizes;
+
+        public Dice(int sizes)
+        {
+            this.Sizes = sizes;
+        }
+
+        public int Sizes
+        {
+            get { return this._sizes; }
+
+            set
+            {
+                if(value < 6)
+                {
+                    throw new Exception("Dice can't have less than 6 sizes");
+                }
+
+                this._sizes = value;
+            }
+        }
+    }
+}
---
+++ 1.DifiningClasses/Live_Demo/StartUp.cs
@@ -0,0 +1,16 @@
+﻿namespace Live_Demo
+{
+    public class StartUp
+    {
+        public static void Main()   
+        {
+            Dice dice = new Dice();
+
+
+            //dice.Sizes = -5;
+            dice.Sizes= 6; 
+
+            System.Console.WriteLine(dice.Sizes);
+        }
+    }
+}

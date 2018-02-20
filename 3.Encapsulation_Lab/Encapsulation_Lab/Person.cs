7020b38194a91e7a85db03f7b43ebf001b863a19
VladislavPenchev <vgpenchev@gmail.com>
2018-02-20 12:45:58 +0200

    3.Encapsulation 2.Salary increase

--- 3.Encapsulation_Lab/Encapsulation_Lab/Person.cs
+++ 3.Encapsulation_Lab/Encapsulation_Lab/Person.cs
@@ -5,12 +5,14 @@
         private string _firstName;
         private string _lastName;
         private int _age;
+        private double _salary;
 
-        public Person(string firstName,string lastName, int age)
+        public Person(string firstName,string lastName, int age, double salary)
         {
             this._firstName = firstName;
             this._lastName = lastName;
             this._age = age;
+            this._salary = salary;
         }
 
         public string FirstName
@@ -29,10 +31,22 @@
             }
         }
 
+        public void IncreaseSalary(double bonus)
+        {
+            if (this.Age < 30)
+            {
+                this._salary += this._salary*((bonus/2) / 100);
+            }
+            else
+            {
+
+                this._salary += this._salary * ((bonus) / 100);
+            }
+        }
 
         public override string ToString()
         {
-            return $"{this.FirstName} {this._lastName} is a {this.Age} years old";
+            return $"{this.FirstName} {this._lastName} get {this._salary:f2} leva";
         }
 
     }
--- 3.Encapsulation_Lab/Encapsulation_Lab/StartUp.cs
+++ 3.Encapsulation_Lab/Encapsulation_Lab/StartUp.cs
@@ -9,18 +9,34 @@
         public static void Main()
         {
 
+            //var lines = int.Parse(Console.ReadLine());
+            //var persons = new List<Person>();
+            //for (int i = 0; i < lines; i++)
+            //{
+            //    var cmdArgs = Console.ReadLine().Split();
+            //    var person = new Person(cmdArgs[0], cmdArgs[1], int.Parse(cmdArgs[2]));
+            //    persons.Add(person);
+            //}
+            //persons.OrderBy(p => p.FirstName)
+            //       .ThenBy(p => p.Age)
+            //       .ToList()
+            //       .ForEach(p => Console.WriteLine(p.ToString()));
+
             var lines = int.Parse(Console.ReadLine());
             var persons = new List<Person>();
             for (int i = 0; i < lines; i++)
             {
                 var cmdArgs = Console.ReadLine().Split();
-                var person = new Person(cmdArgs[0], cmdArgs[1], int.Parse(cmdArgs[2]));
+                var person = new Person(cmdArgs[0],
+                cmdArgs[1],
+                int.Parse(cmdArgs[2]),
+                double.Parse(cmdArgs[3]));
                 persons.Add(person);
             }
-            persons.OrderBy(p => p.FirstName)
-                   .ThenBy(p => p.Age)
-                   .ToList()
-                   .ForEach(p => Console.WriteLine(p.ToString()));
+            double bonus = double.Parse(Console.ReadLine());
+
+            persons.ForEach(p => p.IncreaseSalary(bonus));
+            persons.ForEach(p => Console.WriteLine(p.ToString()));
 
         }
     }

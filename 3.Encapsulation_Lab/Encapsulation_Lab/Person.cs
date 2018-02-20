c3f63605f2eee348ccff1ab2f85b19a5ed7bd6f5
VladislavPenchev <vgpenchev@gmail.com>
2018-02-20 13:21:10 +0200

    3.Encapsulation => 3.Validation Data

--- 3.Encapsulation_Lab/Encapsulation_Lab/Person.cs
+++ 3.Encapsulation_Lab/Encapsulation_Lab/Person.cs
@@ -1,5 +1,7 @@
 ﻿namespace Encapsulation_Lab
 {
+    using System;
+
     public class Person
     {
         private string _firstName;
@@ -9,25 +11,75 @@
 
         public Person(string firstName,string lastName, int age, double salary)
         {
-            this._firstName = firstName;
-            this._lastName = lastName;
-            this._age = age;
-            this._salary = salary;
+            this.FirstName = firstName;
+            this.LastName = lastName;
+            this.Age = age;
+            this.Salary = salary;
         }
 
         public string FirstName
         {
             get
-            {
+            {                
                 return this._firstName;
+            }
+            set
+            {
+                if (value.Length < 3)
+                {
+                    throw new ArgumentException($"{nameof(this.FirstName)} cannot be less than 3 symbols");
+                }
+                this._firstName = value;
+            }
+        }
+
+        public string LastName
+        {
+            get
+            {                
+                return this._lastName;
+            }
+            set
+            {
+                if (value.Length < 3)
+                {
+                    throw new ArgumentException("Last name cannot be less than 3 symbols");
+                }
+                this._lastName = value;
             }
         }
 
         public int Age
         {
             get
-            {
+            {               
                 return this._age;
+            }
+            set
+            {
+                if (value == 0 || value < 0)
+                {
+                    throw new ArgumentException("Age must not be zero or negative");
+                }
+
+                this._age = value;
+            }
+        }
+
+        public double Salary
+        {
+            get
+            {               
+                return this._salary;
+            }
+            set
+            {
+                if (value < 460)
+                {
+                    throw new ArgumentException("Salary can't be less than 460.0");
+                }
+
+                this._salary = value;
             }
         }
 
@@ -46,7 +98,7 @@
 
         public override string ToString()
         {
-            return $"{this.FirstName} {this._lastName} get {this._salary:f2} leva";
+            return $"{this.FirstName} {this._lastName} get {this._salary} leva";
         }
 
     }
--- 3.Encapsulation_Lab/Encapsulation_Lab/StartUp.cs
+++ 3.Encapsulation_Lab/Encapsulation_Lab/StartUp.cs
@@ -22,16 +22,25 @@
             //       .ToList()
             //       .ForEach(p => Console.WriteLine(p.ToString()));
 
+
+            //Task2
             var lines = int.Parse(Console.ReadLine());
             var persons = new List<Person>();
             for (int i = 0; i < lines; i++)
             {
-                var cmdArgs = Console.ReadLine().Split();
-                var person = new Person(cmdArgs[0],
-                cmdArgs[1],
-                int.Parse(cmdArgs[2]),
-                double.Parse(cmdArgs[3]));
-                persons.Add(person);
+                try
+                {
+                    var cmdArgs = Console.ReadLine().Split();
+                    var person = new Person(cmdArgs[0],
+                    cmdArgs[1],
+                    int.Parse(cmdArgs[2]),
+                    double.Parse(cmdArgs[3]));
+                    persons.Add(person);
+                }
+                catch (ArgumentException ex)
+                {
+                    Console.WriteLine(ex.Message);
+                }
             }
             double bonus = double.Parse(Console.ReadLine());
 

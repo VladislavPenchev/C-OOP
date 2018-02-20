011f3035eef4279d684c177fbd5daa8687bb7121
VladislavPenchev <vgpenchev@gmail.com>
2018-02-20 12:05:13 +0200

    3.Encapsulation => 1.SortPerson by Name and Age (Lab)

---
+++ 3.Encapsulation_Lab/Encapsulation_Lab/Person.cs
@@ -0,0 +1,39 @@
+﻿namespace Encapsulation_Lab
+{
+    public class Person
+    {
+        private string _firstName;
+        private string _lastName;
+        private int _age;
+
+        public Person(string firstName,string lastName, int age)
+        {
+            this._firstName = firstName;
+            this._lastName = lastName;
+            this._age = age;
+        }
+
+        public string FirstName
+        {
+            get
+            {
+                return this._firstName;
+            }
+        }
+
+        public int Age
+        {
+            get
+            {
+                return this._age;
+            }
+        }
+
+
+        public override string ToString()
+        {
+            return $"{this.FirstName} {this._lastName} is a {this.Age} years old";
+        }
+
+    }
+}

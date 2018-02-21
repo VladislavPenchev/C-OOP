a1f3cd070ba1bfc160ecbb48c3dff5f0fb7c4b5b
VladislavPenchev <vgpenchev@gmail.com>
2018-02-21 08:55:51 +0200

    3.Encapsulation_Lab => 4.First and Reserved Team

---
+++ 3.Encapsulation_Lab/Encapsulation_Lab/Team.cs
@@ -0,0 +1,47 @@
+﻿namespace Encapsulation_Lab
+{
+    using System.Collections.Generic;
+
+    public class Team
+    {
+        private string _name;
+        private List<Person> _firstTeam;
+        private List<Person> _reserveTeam;
+
+        public Team(string name)
+        {
+            this._name = name;
+            this._firstTeam = new List<Person>();
+            this._reserveTeam = new List<Person>();
+        }
+
+        public string Name
+        {
+            get { return this._name; }
+            set { this._name = value; }
+        }
+
+        public IReadOnlyCollection<Person> FirstTeam
+        {
+            get { return this._firstTeam.AsReadOnly(); }
+        }
+
+        public IReadOnlyCollection<Person> ReserveTeam
+        {
+            get { return this._reserveTeam.AsReadOnly(); }
+        }
+
+        public void AddPerson(Person person)
+        {
+            if (person.Age < 40)
+            {
+                _firstTeam.Add(person);
+            }
+            else
+            {
+                _reserveTeam.Add(person);
+            }
+        }
+
+    }
+}

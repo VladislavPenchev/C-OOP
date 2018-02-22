685ada618dd4d76126eb66568e09fd361591274e
VladislavPenchev <vgpenchev@gmail.com>
2018-02-22 07:47:19 +0200

    4.Encapsulation_exercise => refactoring 1.ClassBox

---
+++ 4.Encapsulation_Exercise/AnimalFarm/ClassBox/Box.cs
@@ -0,0 +1,46 @@
+﻿namespace ClassBox
+{
+    public class Box
+    {
+        private double _length;
+        private double _width;
+        private double _height;
+
+        public Box(double length,double width, double height)
+        {
+            this._length = length;
+            this._width = width;
+            this._height = height;
+        }
+
+
+
+        public double SurfaceArea()
+        {
+            //2lw + 2lh + 2wh
+
+            double surfaceAreaFormula = (2 * this._length * this._width) + (2 * this._length * this._height) + (2 * this._width * this._height);
+
+            return surfaceAreaFormula;
+        }
+
+        public double LeteralSurfaceArea()
+        {
+            //2lh + 2wh
+            double leteralSurfaceAreaForumula = (2 * this._length * this._height) + (2 * this._width * this._height);
+
+            return leteralSurfaceAreaForumula;
+        }
+
+        public double Volume()
+        {
+            // lwh
+            double volumeFormula = this._length * this._width * this._height;
+
+            return volumeFormula;
+        }
+
+        
+
+    }
+}
---
+++ 4.Encapsulation_Exercise/AnimalFarm/ClassBox/StartUp.cs
@@ -0,0 +1,33 @@
+﻿namespace ClassBox
+{
+    using System;
+    using System.Linq;
+    using System.Reflection;
+
+    public class StartUp
+    {
+        public static void Main()
+        {
+            double[] doubleParams = new double[3];
+
+            for (int i = 0; i < doubleParams.Length; i++)
+            {
+                doubleParams[i] = double.Parse(Console.ReadLine());
+            }
+
+
+            Box box = new Box(doubleParams[0], doubleParams[1], doubleParams[2]);           
+
+
+            Type boxType = typeof(Box);
+            FieldInfo[] fields = boxType.GetFields(BindingFlags.NonPublic | BindingFlags.Instance);
+            Console.WriteLine(fields.Count());
+            
+            Console.WriteLine($"Surface Area – {box.SurfaceArea():f2}");
+            Console.WriteLine($"Lateral Surface Area – {box.LeteralSurfaceArea():f2}");
+            Console.WriteLine($"Volume – {box.Volume():f2}");
+
+
+        }
+    }
+}
